0512fa619bc94f9365c39cf92b9289e639925b13
USER <USER@DESKTOP-0QONU6V>
2021-01-22 14:47:11 +0900

    new setting

---
+++ Spring_test/src/main/java/com/spring/test1/TV.java
@@ -0,0 +1,5 @@
+package com.spring.test1;
+
+public interface TV {
+
+}
---
+++ Spring_test/src/main/java/com/spring/test1/TVUser.java
@@ -0,0 +1,14 @@
+package com.spring.test1;
+
+public class TVUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SamsungTV tv = new SamsungTV();
+		tv.powerOn();
+		tv.powerOff();
+		tv.volumeDown();
+		tv.volumeUp();
+	}
+
+}
