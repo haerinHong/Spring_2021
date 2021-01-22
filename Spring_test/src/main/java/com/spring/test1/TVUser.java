fe8488e1757e7864a0bdd2fe5286a4a798e4b3bf
haerinHong <honghabbi9@gmail.com>
2021-01-23 01:26:44 +0900

    ~class3

--- Spring_test/src/main/java/com/spring/test1/TVUser.java
+++ Spring_test/src/main/java/com/spring/test1/TVUser.java
@@ -1,14 +1,33 @@
 package com.spring.test1;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SamsungTV tv = new SamsungTV();
+//		BeanFactory factory = new BeanFactory();
+//		TV tv = (TV)factory.getBean(args[0]);
+//		SamsungTV tv = new SamsungTV();
+//		tv.powerOn();
+//		tv.powerOff();
+//		tv.volumeDown();
+//		tv.volumeUp();
+//		1. Spring 컨테이너를 구동한다.
+		AbstractApplicationContext factory = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+		
+//		2. Spring 컨테이너로 부터 필요한 객체르 요청한다.
+		TV tv = (TV)factory.getBean("tv");
+//		SamsungTV tv = new SamsungTV();
 		tv.powerOn();
 		tv.powerOff();
 		tv.volumeDown();
 		tv.volumeUp();
+		
+//		3. Spring 컨테이너를 종료한다.
+		factory.close();
 	}
 
 }
